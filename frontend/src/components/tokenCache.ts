61a6fbfc06b81ff118757593becb9bd5c749bbce
dkvondee5-beep <dkvondee5@gmail.com>
2026-07-10 17:37:59 +0000

    Add files via upload
    fixed screen iimport

---
+++ frontend/src/components/tokenCache.ts
@@ -0,0 +1,15 @@
+import * as SecureStore from 'expo-secure-store';
+
+export const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return await SecureStore.getItemAsync(key);
+    } catch {
+      await SecureStore.deleteItemAsync(key);
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    return SecureStore.setItemAsync(key, value);
+  },
+};
