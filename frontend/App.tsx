2bac26c7979d6575e7067181252df2b7ed5a6844
dkvondee5-beep <dkvondee5@gmail.com>
2026-07-13 21:13:07 +0000

    Remove tokenCache from ClerkProvider in App component

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -25,7 +25,7 @@ function MainTabs() {
 
 export default function App() {
   return (
-    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY} tokenCache={Platform.OS === 'web' ? undefined : tokenCache}>
+    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <SafeAreaProvider>
           <NavigationContainer>
